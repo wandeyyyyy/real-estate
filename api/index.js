085c9c0a5f919a86f1b26f5df390413d90381a0f
wande <yewandeadeyemi84@gmail.com>
2024-04-01 10:18:24 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/real-estate')
+.then(() => console.log('Connected to MongoDB......'))
+.catch(err => console.log('Could not connect to MongoDB.....', err))
+
 
 const app = express();
 
@@ -6,3 +13,5 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000')
 }
 );
+
+
--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -22,7 +22,7 @@ const Headers = () => {
             <Link to='/about'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
-            <Link to='/signin'>
+            <Link to='/sign-in'>
             <li className=' text-slate-700 hover:underline'>
                 {''}
                 Sign In</li>
