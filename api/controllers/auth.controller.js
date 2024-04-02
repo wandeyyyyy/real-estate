8f507491580877b9974a9da4b8d722fcc7b5b1e9
wande <yewandeadeyemi84@gmail.com>
2024-04-02 15:04:47 +0100

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 const User = require("../model/user.model");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const errorHandler = require("../utils/error");
+const dotenv = require("dotenv");
+dotenv.config()
 
 const signup = async (req,res, next) => {
 const {username, email, password} = req.body;
@@ -40,4 +43,33 @@ next(error)
     
 //     }
 
-module.exports = signup
+
+const signin = async (req,res, next) => {
+    const { email, password} = req.body;
+ 
+    
+    try {
+        //check if email exists 
+        const validUser = await User.findOne({email})
+        if (!validUser) return next(errorHandler(404, 'User not Found'))
+        // check if passord is correct
+        const validPassword = bcrypt.hashSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401 , 'Incorrect Credentials'))
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET );
+    // dont send the password back to client 
+    const{ password: pass, ...rest} = validUser._doc;
+
+
+    res.cookie('access_token', token, {httpOnly: true }).status(200).json(rest)
+
+    } catch (error) {
+    //   next(errorHandler(500, 'error from the function'))
+    next(error)
+    }
+    
+    
+    }
+
+module.exports = {
+                  signin, 
+                  signup}
