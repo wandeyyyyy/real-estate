0d302632d11b8de62f7e457ae23a4565c6987192
wande <yewandeadeyemi84@gmail.com>
2024-04-02 12:12:34 +0100

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const User = require("../model/user.model");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const errorHandler = require("../utils/error");
 
 const signup = async (req,res, next) => {
@@ -10,7 +10,8 @@ const newUser = new User({username, email, password: hashedpassword});
 try {
     
     await newUser.save();
-    res.status(200).json('User created Successfully')
+    res.status(200).json('User created Successfully');
+    res.status(400).json('User already exists')
 } catch (error) {
 //   next(errorHandler(500, 'error from the function'))
 next(error)
@@ -18,5 +19,25 @@ next(error)
 
 
 }
+
+// or use this for more specific error
+// const signup = async (req,res) => {
+//     const {username, email, password} = req.body;
+//     const hashedpassword = bcrypt.hashSync(password, 10);
+//     const newUser = new User({username, email, password: hashedpassword});
+//     let user = await User.findOne({email:req.body.email});
+    
+//     if (user) return res.status(400).json('User already registered');
+    
+        
+//         await newUser.save();
+//         res.status(200).json('User created Successfully');
+    
+    
+    
+    
+    
+    
+//     }
 
 module.exports = signup
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,17 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+
+const [formData, setFormData] = useState({});
+const [error, setError] = useState(null);
+const [loading, setLoading] = useState(false);
+const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+     [e.target.id]: e.target.value,
+    })
+  }
+ // submit to the backend
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        console.log(data);
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+  
+  } catch (error) {
+    setLoading(false);
+    setError(null);
+    navigate('/sign-in');
+  }
+
+ }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-bold my-7'>
         Sign Up
       </h1>
       <form className='flex flex-col gap-4'>
-        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username'/>
-        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email'/>
-        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password'/>
-        <button disabled className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 font-semibold'>Sign Up</button>
+        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+        <button onClick={handleSubmit} disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 font-semibold cursor-pointer'>
+          { loading ? 'Loading... ':'Sign Up'}
+        </button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
@@ -20,7 +62,9 @@ const SignUp = () => {
             Sign In
           </span>
         </Link>
+
       </div>
+         {error &&  <p className='text-red-500 mt-5'>{error}</p>} 
     </div>
   )
 }
