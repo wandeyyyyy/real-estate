4b2ee2a4a3a410ab284997e5b46fc829cee1385d
wande <yewandeadeyemi84@gmail.com>
2024-04-23 19:10:42 +0100

    search api route

--- api/routes/list.route.js
+++ api/routes/list.route.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const {createList, deleteList, updateList, getList} = require('../controllers/list.controller')
+const {createList, deleteList, updateList, getList, getListings} = require('../controllers/list.controller')
 const verifyUser = require('../utils/verifyUser')
 const router = express.Router();
 
 router.post('/create', verifyUser, createList)
 router.delete('/delete/:id', verifyUser, deleteList)
 router.post('/update/:id', verifyUser, updateList)
-router.get('/get/:id',verifyUser,  getList)
+router.get('/get/:id',  getList)
+router.get('/get',  getListings)
+
 
 
 module.exports = router
