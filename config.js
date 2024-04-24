516580def3a6c81297d445a273361e61ad4fd29c
wande <yewandeadeyemi84@gmail.com>
2024-04-24 17:41:46 +0100

    mongodb fixes

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    mongoConnectionString: "mongodb+srv://yewandeadeyemi:yewande@moniq-estate.4vy05g0.mongodb.net/?retryWrites=true&w=majority&appName=moniq-estate"
+}
+
+module.exports = config
